22751de0ad6b8324df16f30fd2f1389283b4ee3f
Justin Thoms <DyneXvX@outlook.com>
2020-07-07 13:10:26 -0400

    Repository setup started - Stopped for lunch

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T:class //have no clue what type of object - just made up T
+    {
+        T Get(int id); //based on id will get a category
+
+        IEnumerable<T> GetAll(                                  
+            Expression<Func<T, bool>> filter = null,            //it we want to retrieve all categories we pass empty
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null                     //if we want to include Properties
+        );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null                     
+        );
+
+        void Add(T entity);
+        void Remove(int id);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
