1fbe1c2829a2faa8bc20f428ce579385fdca857e
Justin Thoms <DyneXvX@outlook.com>
2020-07-10 12:49:02 -0400

    Product Model Built

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+
+        [Key] public int Id { get; set; }
+        public string Title { get; set; }
+        public int Description { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Author { get; set; }
+
+        [Required] [Range(1, 10000)] public double ListPrice { get; set; }
+
+        [Required] [Range(1, 10000)] public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; } //price for the book if done in quality 50 to 99
+
+        [Required] [Range(1, 10000)] public double Price100 { get; set; }
+
+        public string ImageUrl { get; set; }
+        [Required] public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")] public Category Category { get; set; }
+
+        [Required] public int CoverTypeId { get; set; }
+        [ForeignKey("CoverTypeId")] public CoverType CoverType { get; set; }
+
+    }
+}
