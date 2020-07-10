8f68f138367d361af0550aedc723b1805f0ffc50
Justin Thoms <DyneXvX@outlook.com>
2020-07-10 15:41:10 -0400

    Setup the Product Contorller along with the Product View Model

---
+++ BulkyBook.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+    }
+}
