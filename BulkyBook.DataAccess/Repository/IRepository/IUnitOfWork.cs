dc286c8392a0010b6667f90129a4cdfeb1ea6e54
Justin Thoms <DyneXvX@outlook.com>
2020-07-11 15:14:34 -0400

    Company setup complete- Added Index, Upsert and all .js functions Also added Migration for new company model to DB

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+        ICompanyRepository Company { get; }
         ICoverTypeRepository CoverType { get; }
         IProductRepository Product { get; }
 
---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BulkyBook.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string  City { get; set; }
+        public string  State { get; set; }
+        public string PostalCode { get; set; }
+
+        public int? CompanyId { get; set; } //this means this field can be null now!
+
+        [ForeignKey("CompanyId")] public Company Company { get; set; }
+
+        [NotMapped] // will not be pushed to the database.
+        public string  Role { get; set; }
+    }
+}
